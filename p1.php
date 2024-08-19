94135be63879d8fd85d8d6513b95ed3d1e344048
raikk <raik.kh@gmail.com>
2024-08-19 11:15:56 +0530

    new feature

--- p1.php
+++ p1.php
@@ -2,4 +2,14 @@
 
 echo "<h1>hello world</h1>";
 
+echo "<h2>This is php code</h2>";
+echo "<h2>This is php code</h2>";
+echo "<h2>This is php code</h2>";
+echo "<h2>This is php code</h2>";
+$a = 1;
+$b = 100;
+$c = $a + $b;
+echo "<b>Result ".$c."</b>";
+
+
 ?>
